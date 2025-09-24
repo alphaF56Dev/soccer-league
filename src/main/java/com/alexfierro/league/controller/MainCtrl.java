f721389f1294c561b6f3570b879c4e180f284f6a
Alejandro Lopez <aldoss5623@gmail.com>
2025-09-24 11:26:08 -0600

    adding main class to controll session information

---
+++ src/main/java/com/alexfierro/league/controller/MainCtrl.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.alexfierro.league.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.alexfierro.league.entity.UserAccess;
+import com.alexfierro.league.repository.UserAccessRepository;
+import com.alexfierro.league.service.UserAccessService;
+
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * @author alpha
+ *
+ */
+@Controller
+public class MainCtrl {
+	protected UserAccess currentUser = new UserAccess();
+	
+	@Autowired
+	protected HttpSession currentSession;
+	
+	@Autowired
+	protected UserAccessService userSrv;
+	
+	public boolean hasAccess() {
+		boolean access = (currentUser.getUsername() == null);
+		return access;
+	}
+
+	public UserAccess getCurrentUser() {
+		String username = (String)currentSession.getAttribute("username");
+		currentUser = userSrv.findUserByUsername(username);
+		return currentUser;
+	}
+
+	public void setCurrentUser(UserAccess currentUser) {		
+		this.currentUser = currentUser;
+	}
+}
