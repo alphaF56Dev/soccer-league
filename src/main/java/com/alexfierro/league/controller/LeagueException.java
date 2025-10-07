8b0f5deb0300d923d31fbca5cb3431aa21e4eb1e
Alejandro Lopez <aldoss5623@gmail.com>
2025-10-06 18:43:57 -0600

    adding Exception class to do custum exceptions

---
+++ src/main/java/com/alexfierro/league/controller/LeagueException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.alexfierro.league.controller;
+
+/**
+ * @author alpha
+ *
+ */
+public class LeagueException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5047398048349241145L;
+	
+	public LeagueException(String message) {
+		super(message);
+	}
+}
